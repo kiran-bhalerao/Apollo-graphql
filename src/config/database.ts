318a53bfffde2406f0880f1559f9255193e80766
kiranbhalerao123 <bhaleraok65@gmail.com>
2019-05-19 18:20:14 +0530

    configure database host for testing

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,10 +5,10 @@ import User from '../entity/user/user.model'
 mongoose.set('useFindAndModify', false)
 mongoose.set('remove', false)
 
-let { MONGO_HOST, DATABASE_NAME } = process.env
+let { MONGO_HOST, DATABASE_NAME, TEST_DATABASE_URL } = process.env
 
 MONGO_HOST = MONGO_HOST || 'localhost'
-const DATABASE_URL = `mongodb://${MONGO_HOST}/${DATABASE_NAME}`
+const DATABASE_URL = TEST_DATABASE_URL || `mongodb://${MONGO_HOST}/${DATABASE_NAME}`
 
 export const connectToDB = async (done = () => {}) => {
   await mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
